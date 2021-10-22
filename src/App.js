880236cf4404b5bf4dd7eec4d3b08f05eb850a8a
Carlos Andres Charris S <darkklitos@gmail.com>
2021-10-22 13:28:42 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { onAuthStateChanged } from "@firebase/auth";
+import { useEffect, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import { Admin } from "./components/Admin.jsx";
+import { Home } from "./components/Home.jsx";
+import { Login } from "./components/Login.js";
+import { Navbar } from './components/Navbar';
+import { auth } from "./config/firebase.js";
+import { Loading } from './components/Loading';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [firebaseUser, setFirebaseUser] = useState(false)
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (user) => {
+
+            if (user) {
+                const usuario = {
+                    id: user.uid,
+                    email: user.email
+                }
+                console.log(usuario);
+                setFirebaseUser(usuario)
+                console.log('El usuario logueado');
+            } else {
+                console.log('El usuario ya no esta logueado');
+                setFirebaseUser(null)
+            }
+
+        })
+
+
+    }, [setFirebaseUser])
+
+
+    return firebaseUser !== false ? (
+        <Router>
+            <Navbar usuario={firebaseUser} />
+            <div className="container mt-3">
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/admin" component={Admin} />
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </div>
+        </Router>
+    )
+        : <Loading />
 }
 
 export default App;
